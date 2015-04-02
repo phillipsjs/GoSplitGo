1d4ae1e4965cb6b261040798919d4bccec78cb1d
phillipsjs <jonathan.phillips@yale.edu>
2015-04-02 14:46:55 -0400

    setting up basic server and ui files

---
+++ ui.R
@@ -0,0 +1,33 @@
+library(shiny)
+library(ggplot2)
+
+# Define the overall UI
+shinyUI(
+  
+  # Use a fluid Bootstrap layout
+  fluidPage(    
+    
+    # Give the page a title
+    titlePanel("GoSplitGo"),
+    
+    # Generate a row with a sidebar
+    sidebarLayout(      
+      
+      # Define the sidebar with one input
+      sidebarPanel(
+        selectInput("display", "Display:", 
+                    choices=levels(dataset$show),
+                    selected="score"),
+        hr(),
+        helpText("What do you want to see?")
+      ),
+      
+      # Create a spot for the barplot
+      mainPanel(
+        h3(textOutput('caption')),
+        plotOutput("GSGPlot")  
+      )
+      
+    )
+  )
+)
